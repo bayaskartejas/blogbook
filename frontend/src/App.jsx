659f545e07e0085e5a59b97bcf73ab841c3f2211
bayaskartejas <tejasbayaskar8506@gmail.com>
2024-04-07 00:14:53 +0530

    pfp page removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Auth } from './components/Auth'
 import { Homepage } from './components/Homepage'
-import { UploadPfp } from './components/UploadPfp'
-import { ProfilePage } from './components/ProfilePage'
+// import { UploadPfp } from './components/UploadPfp'
+// import { ProfilePage } from './components/ProfilePage'
 import { TechnoStream } from './components/TechnoStream'
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Auth />}></Route>
         <Route path='/home' element={<Homepage />}></Route>
-        <Route path='/profile' element={<ProfilePage />}></Route>
+        {/* <Route path='/profile' element={<ProfilePage />}></Route> */}
         <Route path='/ts' element={<TechnoStream />}></Route>
       </Routes>   
     </BrowserRouter>
